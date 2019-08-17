483fad6900c104234212954becf1f8933e5f2c85
vasrah <vashishtarahul90@gmail.com>
2019-08-18 00:20:51 +0530

    Initial placeholders of Search bar , Video List and Video Detail components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <h1> This is a search bar component </h1>
+        )
+    }
+}
+
+export default SearchBar;
