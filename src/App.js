76f5cf80a12b13246947621e230d092ff2fa7da4
vasrah <vashishtarahul90@gmail.com>
2019-08-28 20:47:05 +0530

    Adding Search Bar and Service

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Grid } from '@material-ui/core';
-import SearchBar from './components/SearchBar';
-import VideoDetail from './components/VideoDetail';
+import { SearchBar, VideoDetail } from './components';
 
-// import youtube from './api/youtube';
+import youtube from './api/youtube';
 
 class App extends React.Component {
+    handleSumbit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: '[API KEY]',
+                q: searchTerm,
+            }
+        });
+
+        console.log(response);
+    }
+
+
     render() {
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12} >
-                            <SearchBar/>
+                            <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8} >
-                            <VideoDetail/>
+                            <VideoDetail />
+
                         </Grid>
                         <Grid item xs={4} >
                             { /* VIDEO */}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { Paper, textField, TextField } from '@material-ui/core';
+
 class SearchBar extends React.Component {
+    state = {
+        searchterm: '',
+    }
+
+    handleChange = (event) => this.setState({ searchterm: event.target.value });
+
+    handleSubmit = (e) => {
+        const { searchterm } = this.state;
+        const { onFormSubmit } = this.props;
+
+        onFormSubmit(searchterm);
+
+        e.preventDefault();
+        
+    }
     render() {
         return (
-            <h1> This is a search bar component </h1>
+            <Paper elevation={6} style={{ padding: '25px' }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search ..." onChange={this.handleChange} />
+                </form>
+            </Paper>
         )
     }
 }
